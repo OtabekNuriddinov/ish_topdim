bccc179a00001624ef2177bf505bba97b896cff9
Otabek Nuriddinov <otabeknuriddinov7808@gmail.com>
2025-06-02 17:21:27 +0500

    [022] theme mode lar yaxshilandi

--- lib/features/home/components/search_bar.dart
+++ lib/features/home/components/search_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import '../../../core/theme/themes.dart';
+import '../../settings/provider/settings_provider.dart';
 
 class CustomSearchBar extends StatelessWidget {
   const CustomSearchBar({super.key});
@@ -10,23 +12,24 @@ class CustomSearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      height: 50,
-      decoration: BoxDecoration(
-          color: Themes.white57,
-          borderRadius: BorderRadius.circular(10)),
-      child: TextField(
-        cursorColor: Themes.orange,
-        style: const TextStyle(color: Themes.black),
-        decoration: InputDecoration(
-            hintText: "Ish Izlash",
-            hintStyle: GoogleFonts.inter(color: Themes.grey),
-            prefixIcon: Icon(
-              CupertinoIcons.search,
-              color: Themes.grey,
-            ),
-            border: InputBorder.none),
-      ),
-    );
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          height: 50,
+          decoration: BoxDecoration(
+              color: Colors.grey.withAlpha(30),
+              borderRadius: BorderRadius.circular(10)),
+          child: TextField(
+            cursorColor: Themes.orange,
+            style: const TextStyle(color: Themes.black),
+            decoration: InputDecoration(
+                hintText: "Ish Izlash",
+                hintStyle: GoogleFonts.inter(color: Themes.grey),
+                prefixIcon: Icon(
+                  CupertinoIcons.search,
+                  color: Themes.grey,
+                ),
+                border: InputBorder.none),
+          ),
+        );
+
   }
 }
